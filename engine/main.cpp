ed029804bdd3c0502172fc45b2236ebc14aabd4b
YogNare <mysticnare@gmail.com>
2025-12-06 18:08:22 +0700

    some info

--- engine/main.cpp
+++ engine/main.cpp
@@ -1,10 +1,5 @@
-#include <QApplication>
-#include <QPushButton>
+#include <iostream>
 
 int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
-    QPushButton button("Hello world!", nullptr);
-    button.resize(200, 100);
-    button.show();
-    return QApplication::exec();
+    std::cout << "Fuck you!!!" << std::endl;
 }
