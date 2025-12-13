46810c8f2d45ce088ee369e626fe0563c984a5d0
YogNare <mysticnare@gmail.com>
2025-12-13 15:53:17 +0700

    some changes

--- cmake-build-debug/.qt/QtDeployTargets.cmake
+++
@@ -1,6 +0,0 @@
-set(__QT_DEPLOY_TARGET_editor_FILE /Users/mikle/Developer/Projects/NSU/Tower-Defence/cmake-build-debug/editor/editor)
-set(__QT_DEPLOY_TARGET_editor_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_engine_FILE /Users/mikle/Developer/Projects/NSU/Tower-Defence/cmake-build-debug/engine/engine)
-set(__QT_DEPLOY_TARGET_engine_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_editor_common_FILE /Users/mikle/Developer/Projects/NSU/Tower-Defence/cmake-build-debug/libeditor_common.a)
-set(__QT_DEPLOY_TARGET_editor_common_TYPE STATIC_LIBRARY)
