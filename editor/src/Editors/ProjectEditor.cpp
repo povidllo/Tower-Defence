829430652456e82a27ec35cd05ad25c756064ffc
Maksim <kuzminovm78@gmail.com>
2025-12-13 13:06:24 +0700

    compile option added

--- editor/src/Editors/ProjectEditor.cpp
+++ editor/src/Editors/ProjectEditor.cpp
@@ -1,8 +1,14 @@
 #include "ProjectEditor.h"
 
 #include <complex>
+#include <QFileDialog>
+#include <QMessageBox>
 #include <qstackedwidget.h>
+#include <qjsonobject.h>
+#include <qjsonarray.h>
+#include <qpainter.h>
 
+#include "TextureManager.h"
 #include "ui_ProjectEditor.h"
 
 ProjectEditor::ProjectEditor(std::string path, std::string name, QWidget *parent) : QMainWindow(parent),
@@ -39,6 +45,7 @@ void ProjectEditor::commonSetUp() {
 	connect(ui->actionEnemy_editor, &QAction::triggered, this, &ProjectEditor::openEnemyEditor);
 	connect(ui->actionMap_editor, &QAction::triggered, this, &ProjectEditor::openMapEditor);
 	connect(ui->actionSave_project, &QAction::triggered, this, &ProjectEditor::onSaveProjectClicked);
+	connect(ui->actionCompile_project, &QAction::triggered, this, &ProjectEditor::compileProjectClicked);
 }
 
 void ProjectEditor::openTowerEditor() {
@@ -62,4 +69,153 @@ void ProjectEditor::onSaveProjectClicked() {
 		return;
 	}
 	qDebug() << "failed to save project";
+}
+
+void ProjectEditor::compileProject() {
+	QString exportDir = QFileDialog::getExistingDirectory(
+		this, "Select Export Folder", QDir::homePath()
+	);
+	if (exportDir.isEmpty()) {
+		return;
+	}
+
+	QDir dir(exportDir);
+	if (!dir.mkpath("resources")) {
+		QMessageBox::critical(this, "Error", "Failed to create resources folder!");
+		return;
+	}
+
+	QSet<QString> usedTextures;
+
+	for (const auto &tower: projectController->getTowers()) {
+		if (!tower->getTowerTexturePath().empty())
+			usedTextures.insert(QString::fromStdString(tower->getTowerTexturePath()));
+		if (!tower->getProjectileTexturePath().empty())
+			usedTextures.insert(QString::fromStdString(tower->getProjectileTexturePath()));
+	}
+
+	for (const auto &enemy: projectController->getEnemies()) {
+		if (!enemy->getEnemyTexturePath().empty()) {
+			usedTextures.insert(QString::fromStdString(enemy->getEnemyTexturePath()));
+		}
+	}
+
+	for (const QString &srcPath: usedTextures) {
+		QFileInfo info(srcPath);
+		QString destPath = dir.filePath("resources/" + info.fileName());
+		if (QFile::exists(destPath)) QFile::remove(destPath);
+		if (!QFile::copy(srcPath, destPath)) {
+			qWarning() << "Failed to copy:" << srcPath;
+		}
+	}
+
+	json compiledMaps = json::array();
+	//
+	for (const auto &map: projectController->getMaps()) {
+		auto mapObj = map->toJson();
+		const auto &tiles = map->getTiles();
+		int tileSize = TextureManager::instance().getImageSize();
+		int width = map->getWidth();
+		int height = map->getHeight();
+
+		QImage atlas(width * tileSize, height * tileSize, QImage::Format_ARGB32);
+		atlas.fill(Qt::transparent);
+
+		QPainter painter(&atlas);
+		for (int y = 0; y < height; ++y) {
+			for (int x = 0; x < width; ++x) {
+				int tileId = tiles[y][x];
+				if (QPixmap tile = TextureManager::instance().get(tileId); !tile.isNull()) {
+					painter.drawPixmap(x * tileSize, y * tileSize, tile);
+				}
+			}
+		}
+		painter.end();
+
+		QString atlasName = QString("map_%1.png").arg(QString::fromStdString(map->getName()));
+		QString atlasPath = dir.filePath(atlasName);
+		if (!atlas.save(atlasPath, "PNG")) {
+			QMessageBox::critical(this, "Error", "Failed to save map atlas: " + atlasName);
+			return;
+		}
+
+		mapObj.erase("tiles");
+		mapObj["finalMapImagePath"] = atlasName.toStdString();
+
+		// json spotsArray = json::array();
+		// for (const auto& spot : map->getSpots()) {
+		//     json spotObj = spot->toJson();
+		//
+		//     if (spotObj.contains("towerTexturePath")) {
+		//         QString oldPath = QString::fromStdString(spot->getTowerTexturePath());
+		//         QFileInfo info(oldPath);
+		//         spotObj["towerTexturePath"] = ("resources/" + info.fileName()).toStdString();
+		//     }
+		//     if (spotObj.contains("projectileTexturePath")) {
+		//         QString oldPath = QString::fromStdString(spot->getProjectileTexturePath());
+		//         QFileInfo info(oldPath);
+		//         spotObj["projectileTexturePath"] = ("resources/" + info.fileName()).toStdString();
+		//     }
+		//
+		//     spotsArray.push_back(spotObj);
+		// }
+		// mapObj["spots"] = spotsArray;
+
+		compiledMaps.push_back(std::move(mapObj));
+	}
+	json compiledTowers = json::array();
+	for (const auto &tower: projectController->getTowers()) {
+		json towerObj = tower->toJson();
+
+		if (towerObj.contains("towerTexturePath")) {
+			QString oldPath = QString::fromStdString(tower->getTowerTexturePath());
+			QFileInfo info(oldPath);
+			towerObj["towerTexturePath"] = ("resources/" + info.fileName()).toStdString();
+		}
+		if (towerObj.contains("projectileTexturePath")) {
+			QString oldPath = QString::fromStdString(tower->getProjectileTexturePath());
+			QFileInfo info(oldPath);
+			towerObj["projectileTexturePath"] = ("resources/" + info.fileName()).toStdString();
+		}
+
+		compiledTowers.push_back(std::move(towerObj));
+	}
+
+	json compiledEnemies = json::array();
+	for (const auto &enemy: projectController->getEnemies()) {
+		json enemyObj = enemy->toJson();
+
+		if (enemyObj.contains("enemyTexturePath")) {
+			QString oldPath = QString::fromStdString(enemy->getEnemyTexturePath());
+			QFileInfo info(oldPath);
+			enemyObj["enemyTexturePath"] = ("resources/" + info.fileName()).toStdString();
+		}
+
+		compiledEnemies.push_back(std::move(enemyObj));
+	}
+
+	json finalProject = projectController->getCurrentProject()->toJson();
+	finalProject["maps"] = compiledMaps;
+	finalProject["towers"] = compiledTowers;
+	finalProject["enemies"] = compiledEnemies;
+	finalProject.erase("textures");
+
+	QString jsonPath = dir.filePath("project.json");
+	std::ofstream file(jsonPath.toStdString());
+	if (file.is_open()) {
+		file << finalProject.dump(4);
+		file.close();
+
+		QMessageBox::information(this, "Success",
+								QString("Project compiled!\n%1 textures + %2 maps → %3")
+								.arg(usedTextures.size())
+								.arg(projectController->getMaps().size())
+								.arg(exportDir));
+	} else {
+		QMessageBox::critical(this, "Error", "Failed to save project.json");
+	}
+}
+
+void ProjectEditor::compileProjectClicked() {
+	compileProject();
 }
