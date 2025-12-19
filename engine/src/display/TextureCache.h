2f312802c231e3948290f93bc5e7a56fbe71f43b
YogNare <mysticnare@gmail.com>
2025-12-20 00:02:06 +0700

    some

--- engine/src/display/TextureCache.h
+++ engine/src/display/TextureCache.h
@@ -5,9 +5,9 @@
 #ifndef TEXTURECACHE_H
 #define TEXTURECACHE_H
 
-#include <unordered_map>
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <unordered_map>
 
 namespace TDEngine {
 	namespace Inner {
@@ -16,21 +16,20 @@ namespace TDEngine {
 			std::unordered_map<std::string, sf::Texture> cache;
 
 		public:
-			sf::Texture& getTexture(const std::string& path) {
+			sf::Texture &getTexture(const std::string &path) {
 				if (cache.find(path) == cache.end()) {
 					sf::Texture tex;
 					if (!tex.loadFromFile(path)) {
 						std::cerr << "Error loading texture: " << path << std::endl;
-						auto er = tex.loadFromFile("2025-12-16 19.50.07.jpg");
+						auto er = tex.loadFromFile("stub.jpg");
 					}
 					cache[path] = tex;
 				}
 				return cache[path];
 			}
 		};
-	}
-}
+	} // namespace Inner
+} // namespace TDEngine
 
 
-
-#endif //TEXTURECACHE_H
+#endif // TEXTURECACHE_H
